f07aea788133d59e32728cb6e9ad5e0eec3d9413
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-01-18 10:51:34 +0000

    Final version

--- 2048.go
+++ 2048.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "math/rand"
     "time"
+    "github.com/nsf/termbox-go"
 )
 
 const (
@@ -193,27 +194,142 @@ func moveDown() bool {
 
     return moved
 }
-
-
-func moveLeft() bool {
-    // Move tiles left and merge if possible
-    // ...
-    return true // or false if no move was made
+func moveTilesLeft(grid [][]int) [][]int {
+    // Iterate through each row
+    for i := 0; i < len(grid); i++ {
+        // Iterate through each column starting from the second column
+        for j := 1; j < len(grid[i]); j++ {
+            // If the current tile is not empty
+            if grid[i][j] != 0 {
+                // Iterate through each column to the left of the current column
+                for k := 0; k < j; k++ {
+                    // If the current tile is empty, move the tile to the left
+                    if grid[i][k] == 0 {
+                        grid[i][k] = grid[i][j]
+                        grid[i][j] = 0
+                        break
+                    }
+                    // If the current tile is equal to the tile to the left, merge them
+                    if grid[i][k] == grid[i][j] {
+                        grid[i][k] *= 2
+                        grid[i][j] = 0
+                        break
+                    }
+                }
+            }
+        }
+    }
+    // Move the tiles left again after the merge
+    grid = moveTilesLeft(grid)
+    return grid
 }
 
-func moveRight() bool {
-    // Move tiles right and merge if possible
-    // ...
-    return true // or false if no move was made
+func moveTilesRight(grid [][]int) (bool, [][]int) {
+    // variable to keep track if move was made
+    moveMade := false
+    // Iterate through each row
+    for i := 0; i < len(grid); i++ {
+        // Iterate through each column starting from the second to last column
+        for j := len(grid[i]) - 2; j >= 0; j-- {
+            // If the current tile is not empty
+            if grid[i][j] != 0 {
+                // Iterate through each column to the right of the current column
+                for k := len(grid[i]) - 1; k > j; k-- {
+                    // If the current tile is empty, move the tile to the right
+                    if grid[i][k] == 0 {
+                        grid[i][k] = grid[i][j]
+                        grid[i][j] = 0
+                        moveMade = true
+                        break
+                    }
+                    // If the current tile is equal to the tile to the right, merge them
+                    if grid[i][k] == grid[i][j] {
+                        grid[i][k] *= 2
+                        grid[i][j] = 0
+                        moveMade = true
+                        break
+                    }
+                }
+            }
+        }
+    }
+    return moveMade, grid
 }
 
-
-func checkGameOver() {
-    // Check if the game is over (i.e. the board is full and no more moves are possible)
-    // ...
+func isGameOver(grid [][]int) bool {
+    // Check if the board is full
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[i]); j++ {
+            if grid[i][j] == 0 {
+                return false
+            }
+        }
+    }
+    // Check if any moves are possible
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[i]); j++ {
+            if i > 0 && grid[i-1][j] == grid[i][j] {
+                return false
+            }
+            if i < len(grid)-1 && grid[i+1][j] == grid[i][j] {
+                return false
+            }
+            if j > 0 && grid[i][j-1] == grid[i][j] {
+                return false
+            }
+            if j < len(grid[i])-1 && grid[i][j+1] == grid[i][j] {
+                return false
+            }
+        }
+    }
+    return true
 }
 
-func drawBoard() {
-    // Draw the current state of the board to the terminal
-    // ...
+func drawBoard(grid [][]int) {
+    // Initialize the termbox library
+    err := termbox.Init()
+    if err != nil {
+        panic(err)
+    }
+    defer termbox.Close()
+
+    // Iterate through each row
+    for i := 0; i < len(grid); i++ {
+        // Iterate through each column
+        for j := 0; j < len(grid[i]); j++ {
+            // Get the value of the current tile
+            value := grid[i][j]
+            // Set the color of the tile based on its value
+            var fg, bg termbox.Attribute
+            switch {
+            case value == 0:
+                fg, bg = termbox.ColorDefault, termbox.ColorDefault
+            case value < 4:
+                fg, bg = termbox.ColorWhite, termbox.ColorBlack
+            case value < 8:
+                fg, bg = termbox.ColorYellow, termbox.ColorBlack
+            case value < 16:
+                fg, bg = termbox.ColorGreen, termbox.ColorBlack
+            case value < 32:
+                fg, bg = termbox.ColorCyan, termbox.ColorBlack
+            case value < 64:
+                fg, bg = termbox.ColorMagenta, termbox.ColorBlack
+            case value < 128:
+                fg, bg = termbox.ColorBlue, termbox.ColorBlack
+            case value < 256:
+                fg, bg = termbox.ColorRed, termbox.ColorBlack
+            default:
+                fg, bg = termbox.ColorBlack, termbox.ColorWhite
+            }
+
+            // Draw the current tile
+            if value != 0 {
+                termbox.SetCell(j, i, ' ', fg, bg)
+                fmt.Fprintf(termbox.CellBuffer(), "%4d", value)
+            } else {
+                termbox.SetCell(j, i, ' ', termbox.ColorDefault, termbox.ColorDefault)
+            }
+        }
+    }
+    termbox.Flush()
 }
